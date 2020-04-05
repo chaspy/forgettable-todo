dfd711982b52e86826cddddeeb76355d50b55fb3
kondo takeshi <chaspy@users.noreply.github.com>
2020-04-05 13:05:27 +0900

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i(github)
 
   def self.create_unique_string
     SecureRandom.uuid
