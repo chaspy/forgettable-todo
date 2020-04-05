c71c36d5706ae948c92d8e28002902664e5c7d1b
kondo takeshi <chaspy@users.noreply.github.com>
2020-04-06 06:26:37 +0900

    Redirect to root after creating todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -24,7 +24,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Todo was successfully created.' }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
