e7a7ca457608b3236cc08e3a42fbcb522ed09c7f
kondo takeshi <chaspy@users.noreply.github.com>
2020-04-05 17:48:04 +0900

    rails g migration AddColumnToUser name:string provider:string uid:string

---
+++ db/migrate/20200405081326_add_column_to_user.rb
@@ -0,0 +1,7 @@
+class AddColumnToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+  end
+end
