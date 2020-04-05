f2391bbd380ba9fd8e44ee02d889bd094f5f0019
kondo takeshi <chaspy@users.noreply.github.com>
2020-04-05 13:02:51 +0900

    Set random value to uid

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,2 +1,6 @@
-class Users::RegistrationsController < ApplicationController
+class Users::RegistrationsController < Devise::RegistrationsController
+  def build_resource(hash={})
+    hash[:uid] = User.create_unique_string
+    super
+  end
 end
