8aa2745d4eb4ae9f000c1b8db9b6633d38337830
kondo takeshi <chaspy@users.noreply.github.com>
2020-04-05 13:06:17 +0900

    Add method for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,23 @@ class User < ApplicationRecord
   def self.create_unique_string
     SecureRandom.uuid
   end
+
+  def self.find_for_github_oauth(auth, signed_in_resource=nil)
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+
+    unless user
+      user = User.new(provider: auth.provider,
+                      uid:      auth.uid,
+                      name:     auth.info.name,
+                      email:    User.dummy_email(auth),
+                      password: Devise.friendly_token[0, 20]
+      )
+    end
+    user.save
+    user
+  end
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
