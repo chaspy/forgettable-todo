9bdfdeecd56ded0e0e6c669ac8e6e5586a032b22
kondo takeshi <chaspy@users.noreply.github.com>
2020-04-07 02:45:10 +0900

    Move logic to be showing todos to model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,11 @@
 class Todo < ApplicationRecord
   belongs_to :user
+
+  def list_valid_todos(current_user)
+    # Valid TODO
+    # 1. Own TODO
+    # 2. It is NOT finished
+    # 3. Within 8 days(691200 seconds) after being created
+    self.user_id == current_user.id && !self.finished && Time.zone.now - self.created_at < 691200
+  end
 end
