b8b5f327fc93deb4a11e45875411f3e3f3a94e0b
kondo takeshi <chaspy@users.noreply.github.com>
2020-04-25 11:44:23 +0900

    Separate run script to build and run part

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -eu
+docker-compose build
+docker-compose run web bin/rake db:create db:migrate db:seed
--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 set -eu
-docker-compose build
-docker-compose run web bin/rake db:create db:migrate db:seed
 docker-compose up
