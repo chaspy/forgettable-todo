7a667a26fb7a32caf9502040fae84540043f266e
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2020-04-26 02:44:55 +0000

    Add type declaration
    - Resonse: Get method
    - Request: Post method

---
+++ app/javascript/type.tsx
@@ -0,0 +1,16 @@
+export type Response = {
+    user_id: Number
+    title: string
+    description: string
+    finished: Boolean
+    created_at: Date
+    updated_at: Date
+}
+export type Request = {
+    user_id: Number
+    title: string
+    description: string
+    finished: Boolean
+    created_at: Date
+    updated_at: Date
+}
