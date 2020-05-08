3637284a390a84f0da53bc5dd076c821bc1f7654
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2020-05-08 10:02:55 +0900

    Create and configure store

--- app/javascript/packs/hello_react.tsx
+++ app/javascript/packs/hello_react.tsx
@@ -2,13 +2,22 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from './app'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from '../reducers'
+
+const store = createStore(reducer)
+
+
 interface HelloProps {
   name: string;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.body.appendChild(document.createElement("div")),
   )
 })
