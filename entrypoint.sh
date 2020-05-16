fb07eebba6ac1e57ca05ecf82ed24ae435b03680
kondo takeshi <chaspy@users.noreply.github.com>
2020-05-16 17:32:46 +0900

    Precompile

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# For production
+bundle exec rake assets:precompile
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
